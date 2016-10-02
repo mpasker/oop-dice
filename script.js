186ee271c25b69d9d59f1a78f5f025bca48102a6
Matt Pasker <matthewpasker@Matthews-MacBook-Pro.local>
2016-10-02 10:40:33 -0500

    Added roll function to Dice object and click function to roll all existing dice.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-    // $('#button-container').append('<button id="add-dice">Add Dice!</button>');
-    // $('#button-container').append('<button id="roll-die">Roll Die!</button>');
     $('#add-dice').click(function() {
         return newDice();
-    })
-    
+    });
+    $('#roll-dice').click(function() {
+        return rollDice();
+    });
+
     //array to store loaded dice.
     var dieArray = [];
     //create Dice object that gives ID and value.
@@ -13,15 +14,24 @@ $(document).ready(function() {
         this.value = Math.floor((Math.random() * 6) + 1);
         this.generate = function() {
             var d = $('<div id =' + this.elementID + ' class="dice"></div>');
-            $('#dice-containter').append(d);
+            $('#dice-container').append(d);
+            $('#' + this.elementID).text(this.value.toString());
+        };
+        this.roll = function() {
+            this.value = Math.floor((Math.random() * 6) + 1);
             $('#' + this.elementID).text(this.value.toString());
         }
     };
-     function newDice() {
+    function newDice() {
         var dice = new Dice(dieArray.length);
         dice.generate();
         dieArray.push(dice);
-    };
+    }
+    function rollDice() {
+        for(i = 0; i < dieArray.length; i++) {
+            dieArray[i].roll();
+        }
+    }
     
 });
 
