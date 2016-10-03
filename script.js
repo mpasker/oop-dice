1832fa2f9072a9456d64a90d00f3ab4f917f3c00
Matt Pasker <matthewpasker@Matthews-MacBook-Pro.local>
2016-10-03 14:21:33 -0500

    added function to calculate sum of all dice on screen

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
     $('#add-dice').click(function() {
         return newDice();
-    });
+    })
     $('#roll-dice').click(function() {
         return rollDice();
-    });
+    })
+    $('#sum-dice').click(function() {
+        return sumDice();
+    })
 
     //array to store loaded dice.
     var dieArray = [];
+    console.log(dieArray);
     //create Dice object that gives ID and value.
     var Dice = function(elementID) {
         this.elementID = elementID;
@@ -27,10 +31,18 @@ $(document).ready(function() {
         dice.generate();
         dieArray.push(dice);
     }
+    //Loops through array of die to roll all dice loaded on page.
     function rollDice() {
         for(i = 0; i < dieArray.length; i++) {
             dieArray[i].roll();
         }
+    }
+    function sumDice() {
+        var sum = 0;
+        for(i = 0; i < dieArray.length; i++) {
+            sum += dieArray[i].value;
+        }
+        return alert("The sum of all die on the screen is equal to" + " " + sum);
     }
     
 });
